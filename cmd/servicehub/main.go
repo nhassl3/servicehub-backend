099d761ffaf4caf575ff99abcacd4a44dc50fccc
nhassl3 <matvey.kvasov.05@mail.ru>
2026-07-24 11:45:52 +0300

    refactor: add must load functions in access point

--- cmd/servicehub/main.go
+++ cmd/servicehub/main.go
@@ -3,13 +3,14 @@ package main
 import (
 	"log"
 
+	"github.com/nhassl3/servicehub-backend/cmd"
 	"github.com/nhassl3/servicehub-backend/internal/app"
 	"go.uber.org/zap"
 )
 
 func main() {
-	cfg := MustLoadConfig()
-	zapLogger := MustLoadLogger(cfg.Log.Level)
+	cfg := cmd.MustLoadConfig()
+	zapLogger := cmd.MustLoadLogger(cfg.Log.Level)
 	defer func(log *zap.Logger) {
 		err := log.Sync()
 		if err != nil {
