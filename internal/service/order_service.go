9c2e72265d52dd848dfcae41fb54702b70a74875
nhassl3 <matvey.kvasov.05@mail.ru>
2026-07-26 22:08:36 +0300

    Silently added receiving email from context or Redis for transfer to a new payload due to a change in 7a414d1

--- internal/service/order_service.go
+++ internal/service/order_service.go
@@ -5,17 +5,19 @@ import (
 	"fmt"
 
 	"github.com/nhassl3/servicehub-backend/internal/domain"
+	"github.com/nhassl3/servicehub-backend/internal/transport/grpc/interceptors"
 	"go.uber.org/zap"
 )
 
 type OrderService struct {
 	eventPublisher domain.EventPublisher
 	orderRepo      domain.OrderRepository
+	userRedis      domain.UserRedis
 	log            *zap.Logger
 }
 
-func NewOrderService(orderRepo domain.OrderRepository, publisher domain.EventPublisher, logger *zap.Logger) *OrderService {
-	return &OrderService{eventPublisher: publisher, orderRepo: orderRepo, log: logger}
+func NewOrderService(orderRepo domain.OrderRepository, publisher domain.EventPublisher, userRedis domain.UserRedis, logger *zap.Logger) *OrderService {
+	return &OrderService{eventPublisher: publisher, orderRepo: orderRepo, userRedis: userRedis, log: logger}
 }
 
 // CreateOrder creates order and in parallel send to notifier service message with notifying user action.
@@ -26,12 +28,27 @@ func (s *OrderService) CreateOrder(ctx context.Context, username string) (*domai
 		return nil, fmt.Errorf("order_service.CreateOrder: failed to create order (call checkout): %w", err)
 	}
 
-	if err = s.eventPublisher.PublishOrderCreated(ctx, domain.OrderCreatedPayload{
-		OrderUID: order.ID,
-		Username: order.Username,
-		Total:    order.TotalAmount,
-	}); err != nil {
-		s.log.Error("failed to publish order.created event", zap.Error(err))
+	{
+		var email string
+		payload, ok := interceptors.PayloadFromContext(ctx)
+		if !ok {
+			user, err := s.userRedis.Profile(ctx, username)
+			if err != nil {
+				s.log.Error("order_service.CreateOrder: failed to get user from redis", zap.Error(err))
+			}
+			email = user.Email
+		} else {
+			email = payload.Email
+		}
+
+		if err = s.eventPublisher.PublishOrderCreated(ctx, domain.OrderCreatedPayload{
+			Email:    email,
+			OrderUID: order.ID,
+			Username: username,
+			Total:    order.TotalAmount,
+		}); err != nil {
+			s.log.Error("failed to publish order.created event", zap.Error(err))
+		}
 	}
 
 	return order, nil
