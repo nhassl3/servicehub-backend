3d2e4fee03509e780d5d74b55e8fd51f03f61d25
nhassl3 <matvey.kvasov.05@mail.ru>
2026-07-27 10:14:59 +0300

    feat: add kafka to balance update

--- internal/service/balance_service.go
+++ internal/service/balance_service.go
@@ -2,16 +2,27 @@ package service
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/nhassl3/servicehub-backend/internal/domain"
+	"github.com/nhassl3/servicehub-backend/internal/transport/grpc/interceptors"
+	"go.uber.org/zap"
 )
 
 type BalanceService struct {
-	repo domain.BalanceRepository
+	eventPublisher domain.EventPublisher
+	userRedis      domain.UserRedis
+	repo           domain.BalanceRepository
+	log            *zap.Logger
 }
 
-func NewBalanceService(repo domain.BalanceRepository) *BalanceService {
-	return &BalanceService{repo: repo}
+func NewBalanceService(
+	repo domain.BalanceRepository,
+	eventPublisher domain.EventPublisher,
+	userRedis domain.UserRedis,
+	log *zap.Logger,
+) *BalanceService {
+	return &BalanceService{repo: repo, eventPublisher: eventPublisher, userRedis: userRedis}
 }
 
 func (s *BalanceService) GetBalance(ctx context.Context, username string) (*domain.Balance, error) {
@@ -22,7 +33,35 @@ func (s *BalanceService) Deposit(ctx context.Context, username string, amount fl
 	if amount <= 0 {
 		return nil, domain.ErrInvalidInput
 	}
-	return s.repo.Deposit(ctx, username, amount)
+	balance, err := s.repo.Deposit(ctx, username, amount)
+	if err != nil {
+		return nil, fmt.Errorf("balance_service.Deposit: failed to deposit amount on user balance: %w", err)
+	}
+
+	s.log.Info("AMOUNT COMPARE", zap.Float64("amount", amount), zap.Float64("balance", balance.Amount))
+
+	{
+		var email string
+		payload, ok := interceptors.PayloadFromContext(ctx)
+		if !ok {
+			user, err := s.userRedis.Profile(ctx, username)
+			if err != nil {
+				s.log.Warn("balance_service.Deposit: failed to get user profile", zap.Error(err))
+			}
+			email = user.Email
+		} else {
+			email = payload.Email
+		}
+		if err = s.eventPublisher.PublishBalanceUpdated(ctx, domain.BalanceUpdatedPayload{
+			Email:    email,
+			Username: username,
+			Amount:   balance.Amount,
+		}); err != nil {
+			s.log.Warn("balance_service.Deposit: failed to publish balance updated event", zap.Error(err))
+		}
+	}
+
+	return balance, nil
 }
 
 func (s *BalanceService) GetTransactionHistory(ctx context.Context, params domain.ListTransactionsParams) ([]domain.BalanceTransaction, int64, error) {
